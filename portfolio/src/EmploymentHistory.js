29812780184721332f791103590009ee1ee04d00
Spencer Saunders <SpencerSaunders@u.boisestate.edu>
2023-03-12 22:12:37 -0600

    Added employment timeline section

--- portfolio/src/EmploymentHistory.js
+++ portfolio/src/EmploymentHistory.js
@@ -3,12 +3,14 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import BoozAllenHamiltonLogo from "./booz.jpg";
 
 const EmploymentHistory = () => {
   return (
-    <div className='employment-history'>
+    <div className="employment-history">
       <VerticalTimeline>
         <VerticalTimelineElement
+          icon={<img src={BoozAllenHamiltonLogo} />}
           className="vertical-timeline-element--work"
           contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
           contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
@@ -69,33 +71,6 @@ const EmploymentHistory = () => {
           <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
           <p>Strategy, Social Media</p>
         </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="November 2012"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Agile Development Scrum Master
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">Certification</h4>
-          <p>Creative Direction, User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2002 - 2006"
-          iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
-        >
-          <h3 className="vertical-timeline-element-title">
-            Bachelor of Science in Interactive Digital Media Visual Imaging
-          </h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Bachelor Degree
-          </h4>
-          <p>Creative Direction, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
-        />
       </VerticalTimeline>
     </div>
   );
