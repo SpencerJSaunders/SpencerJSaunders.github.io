1ed585351e687b1a5a47c858e9e91480e7d141c8
Spencer Saunders <SpencerSaunders@u.boisestate.edu>
2023-03-18 15:45:53 -0600

    Removed unnecessary root folder

---
+++ src/PortfolioHeader.js
@@ -0,0 +1,39 @@
+import "./styles/header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+const PortfolioHeader = () => {
+  return (
+    <header>
+      <div className="portfolio-banner">
+        <div className="portfolio-banner__socials">
+          <a href="https://github.com">
+            <FontAwesomeIcon size="4x" icon={faGithub} />
+          </a>
+          <a href="https://linkedin.com">
+            <FontAwesomeIcon size="4x" icon={faEnvelope} />
+          </a>
+          <a href="https://linkedin.com">
+            <FontAwesomeIcon size="4x" icon={faLinkedin} />
+          </a>
+          <a href="./resume.pdf" download="Spencer Saunders Resume">
+            Download my Resume
+          </a>
+        </div>
+        <div className="portfolio-banner__text-container">
+          <div>
+            <h2 className="fade-in portfolio-banner__text-container__name">
+              Spencer Saunders
+            </h2>
+            <p className="fade-in portfolio-banner__text-container__occupation">
+              Front End Developer
+            </p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PortfolioHeader;
