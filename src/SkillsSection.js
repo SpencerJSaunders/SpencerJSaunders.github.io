9976be07f288c1756cad747a4315d3888cf82a3a
Spencer Saunders <SpencerSaunders@u.boisestate.edu>
2023-03-21 20:40:49 -0600

    Changed class names for skills section

--- src/SkillsSection.js
+++ src/SkillsSection.js
@@ -4,42 +4,42 @@ import { faPeopleGroup, faMicroscope, faUniversalAccess } from "@fortawesome/fre
 
 const SkillsSection = () => {
   return (
-    <section className="section">
+    <section className="section skills-section">
       <div className="container">
         <h2 className="section__heading">Skills</h2>
         <p className="section__subheading">
           Skills including programming languages, libraries, tools, and development
           environments
         </p>
-        <div className="skills-list">
-          <div className="skills-list__skill">
+        <div className="skills-section__row">
+          <div className="skills-section__item">
             <FontAwesomeIcon size='7x' icon={faReact}/>
             <h4>React</h4>
             <p>3+ years of experience with React including React libraries like React Router, React Redux, and Mobx React Form.</p>
           </div>
-          <div className="skills-list__skill">
+          <div className="skills-section__item">
           <FontAwesomeIcon size='7x' icon={faGit}/>
             <h4>Version Control</h4>
             <p>5+ years of experience working with git version control. Experience using GitHub, GitLab, and BitBucket for verion control.</p>
           </div>
-          <div className="skills-list__skill">
+          <div className="skills-section__item">
           <FontAwesomeIcon size='7x' icon={faJs}/>
             <h4>Programming Languages</h4>
             <p>5+ years of experience with HTML, CSS, Sass, and JavaScript. Also have experience with TypeScript. Some exposure to Python and Java.</p>
           </div>
         </div>
-        <div className="skills-list" style={{marginTop: '3rem'}}>
-          <div className="skills-list__skill">
+        <div className="skills-section__row" style={{marginTop: '3rem'}}>
+          <div className="skills-section__item">
           <FontAwesomeIcon size='7x' icon={faPeopleGroup}/>
             <h4>Agile</h4>
             <p>4+ years of experience working in Agile environments with developers, stakeholders, business analysts, and designers.</p>
           </div>
-          <div className="skills-list__skill">
+          <div className="skills-section__item">
            <FontAwesomeIcon size='7x' icon={faUniversalAccess}/>
             <h4>Accessibility</h4>
             <p>Experience using Axe, Jaws, and Semantic HTML to create accessible 508-compliant websites.</p>
           </div>
-          <div className="skills-list__skill">
+          <div className="skills-section__item">
             <FontAwesomeIcon size='7x' icon={faMicroscope} />
             <h4>Unit Testing</h4>
             <p>Experience writing unit tests with Jest and React Testing Library to reach full code coverage.</p>
