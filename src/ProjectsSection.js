688aa940f3ec3705fd39a03f271ba365813f9dba
Spencer Saunders <SpencerSaunders@u.boisestate.edu>
2023-12-21 17:35:57 -0700

    Removed divs with title attribute from development logos

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -19,10 +19,7 @@ const ProjectsSection = () => {
             <img src={require("./imgs/calendar.png")} alt="Habit Tracker" />
             <h4>Habit Tracker</h4>
             <div className="projects-section__development-icons">
-              {/* TODO: Add title attribute to all development icons */}
-              <div title="React">
-                <ReactOriginal size={50} />
-              </div>
+              <ReactOriginal size={50} />
               <NextjsOriginalWordmark size={50} />
               <SassOriginal size={50} />
               <ReduxOriginal size={50} />
